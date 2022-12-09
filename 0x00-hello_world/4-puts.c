33a5009c909e87870f79be92e09b5f58eac06b76
Alia <aliachiemena@gmail.come>
2022-12-09 09:51:42 -0800

    Put

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
+
 /**
- * main - Entry point
- * Return: Always Return 0 (success)
- *
- */
-int main(void)
+ *  * main - prints the phrase "Programming is like building a multilingual puzzle"
+ *   * Return: 0 if exited properly, non-zero otherwise
+ *    */
+int main(void) 
 {
-	puts("\"programming is like building a multilingual puzzle");
-	Return (0);
+	    puts("\"Programming is like building a multilingual puzzle");
+	        return (0);
 }
